16668766f871283c663644c0964fed13e368deab
Barnaby Malet <bmalet@gmail.com>
2014-07-10 14:29:14 +0100

    pump vanilla web socket messages through to socket.io message bus

--- index.js
+++ index.js
@@ -10,30 +10,31 @@ server.listen(8080);
 
 console.log('web server listening on port 8080...');
 
+//
+// Socket.io message bus
+//
+
+// specify all events that should be relayed here
 var events = [
   'video:play',
   'image:switch'
 ];
 
 io.on('connection', function (socket) {
-  console.log('new socket connection...');
+  console.log('new socket.io connection...');
   events.forEach(function(event){
-    console.log('listening for', event);
     socket.on(event, function (data) {
-      console.log('Received event', event);
+      console.log('socket.io recieved', event);
       socket.broadcast.emit(event);
     });  
   });
 });
 
 
-
-// Websocket shit
-
-var server = http.createServer(function(request, response) {
-    response.writeHead(404);
-    response.end();
-});
+//
+// Vanilla WebSocket Server
+//
+var server = http.createServer();
 
 server.listen(8081, function() {
     console.log((new Date()) + ' SocketServer is listening on port 8081');
@@ -41,27 +42,22 @@ server.listen(8081, function() {
 
 var wsServer = new WebSocketServer({
     httpServer: server,
-    // You should not use autoAcceptConnections for production
-    // applications, as it defeats all standard cross-origin protection
-    // facilities built into the protocol and the browser.  You should
-    // *always* verify the connection's origin and decide whether or not
-    // to accept it.
     autoAcceptConnections: false
 });
-
-
 
 wsServer.on('request', function(request) {
 
     var connection = request.accept();
 
-    console.log((new Date()) + ' new connection');
+    console.log((new Date()) + ' new websocket connection');
     connection.on('message', function(message) {
         if (message.type === 'utf8') {
-            console.log('Received Message: ' + message.utf8Data);
+            console.log('Received WebSocket Message: ' + message.utf8Data);
+            io.sockets.emit(message.utf8Data);
         }
         else if (message.type === 'binary') {
-            console.log('Received Binary Message of ' + message.binaryData.length + ' bytes');
+          // We don't support this
+          console.log('Received WebSocket Binary Message of ' + message.binaryData.length + ' bytes');
         }
     });
     connection.on('close', function(reasonCode, description) {
